53076b4b176adab4b922aa4180679652784d5270
hhjoo <joohhcse@gmail.com>
2024-05-21 15:16:49 +0900

    Init

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(
+      MaterialApp(
+        home: Scaffold(
+          body: Text('project init'),
+        ),
+      ),
+  );
+}
